2f4727408d125d92fe42042df17bd3dcd5d3a91c
Suliang1027 <sl903dj@hotmail.com>
2023-11-26 13:11:13 +0800

    suckless

---
+++ dwm/scripts/volumetoggle.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# 只静音 Master，不静音Speaker
+amixer -q sset Master toggle
+amixer -q sset Speaker unmute
+
+# 不静音Master，切换Speaker
+# amixer -q sset Master unmute
+# amixer -q sset Speaker toggle
