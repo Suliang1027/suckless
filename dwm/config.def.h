f9a8ce8c3957d5df528abd1f21aeaceabbc2429a
Suliang1027 <sl903dj@hotmail.com>
2023-12-01 13:52:43 +0800

    修改dwm顶栏字体为Hack Nerd Font:Regular

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -12,8 +12,7 @@ static const int showsystray        = 1;        /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const Bool viewontag	    = True;	/* Switch view on tag switch */
-/* static const char *fonts[]          = { "Hack Nerd Font:style=Regular:size=10", "Microsoft YaHei:style=Regular:size=10" }; */
-static const char *fonts[]          = { "文泉驿微米黑:style=Regular:size=10" };
+static const char *fonts[]          = { "Hack Nerd Font:style=Regular:size=10", "Microsoft YaHei:style=Regular:size=10" };
 static const char dmenufont[]       = "Hack Nerd Font:style=Regular:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
